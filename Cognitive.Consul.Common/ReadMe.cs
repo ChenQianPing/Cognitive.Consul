573744abb9e1ca560ec1c01dc9c27e1acb4f3695
chenqp <13542427@qq.com>
2018-08-10 09:04:01 +0800

    first Commit

--- Cognitive.Consul.Common/ReadMe.cs
+++ Cognitive.Consul.Common/ReadMe.cs
@@ -2,6 +2,9 @@
  * 
  * # 基于Consul实现服务治理
  * 　
+ * https://www.consul.io/downloads.html
+ * consul members
+ * 
  * 通过UI查看集群
  * Consul不仅提供了丰富的命令查看集群情况，还提供了一个WebUI，默认端口8500，我们可以通过访问这个URL
  * http://127.0.0.1:8500
